9deee7d8d4cd6873e22b3bbfb30a72e6d40f223c
Cedric Angelo Plasabas <cedricangelo.plasabas@gmail.com>
2018-12-08 12:45:33 +0800

    new cert

--- src/app.js
+++ src/app.js
@@ -31,9 +31,9 @@ var transporter = nodemailer.createTransport({
 
 require('./routes')(app)
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/admin.davohjewelryco.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/admin.davohjewelryco.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/admin.davohjewelryco.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.davohjewelryco.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api.davohjewelryco.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api.davohjewelryco.com/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
