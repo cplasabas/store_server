4ea872490a2ff1d5943e4d24e3a508d2581ae2a5
Cedric Plasabas <cedricangelo.plasabas@gmail.com>
2019-09-28 16:32:13 +0800

    fix

--- src/policies/ExpenseControllerPolicy.js
+++ src/policies/ExpenseControllerPolicy.js
@@ -3,10 +3,10 @@ const joi = require("joi")
 module.exports = {
 	create (req,res,next) {
 		const schema = {
-			or_number: joi.string(),
+			or_number: joi.string().allow(''),
 			name: joi.string(),
 			description: joi.string().allow(''),
-			tin: joi.string(),
+			tin: joi.string().allow(''),
 			amount: joi.number(),
 			or_type: joi.number().integer().allow(null).optional(),
 			type: joi.number().integer(),
